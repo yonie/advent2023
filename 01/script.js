34cf81ec9bbb359584d054d8a2b5780785e10154
Ronald <yonie@yonie.org>
2023-12-04 14:12:17 +0100

    prettier

--- 01/script.js
+++ 01/script.js
@@ -1,64 +1,65 @@
-const startTime = performance.now();
+const startTime = performance.now()
 
-const readline = require("readline");
-const fs = require("fs");
+const readline = require('readline')
+const fs = require('fs')
 
-const fileStream = fs.createReadStream("input");
+const fileStream = fs.createReadStream('input')
 
 const rl = readline.createInterface({
-  input: fileStream,
-});
+  input: fileStream
+})
 
-let [sum, sum2] = [0, 0];
+let [sum, sum2] = [0, 0]
 
-rl.on("line", (line) => {
-  sum += getCalibrationValue(line);
-  sum2 += getCalibrationValue(line, true);
-});
+rl.on('line', line => {
+  sum += getCalibrationValue(line)
+  sum2 += getCalibrationValue(line, true)
+})
 
-rl.on("close", () => {
-  console.log("answer:", sum, sum2);
-  const endTime = performance.now();
-  const runtimeMs = endTime - startTime;
-  console.log(`runtime: ${runtimeMs} ms`);
-});
+rl.on('close', () => {
+  console.log('answer:', sum, sum2)
+  const endTime = performance.now()
+  const runtimeMs = endTime - startTime
+  console.log(`runtime: ${runtimeMs} ms`)
+})
 
-function getCalibrationValue(line, includeStrings) {
-  regex = /\d/g;
-  if (includeStrings)
-    regex = /\d|one|two|three|four|five|six|seven|eight|nine/g;
+function getCalibrationValue (line, includeStrings) {
+  let regex = /\d/g
+  if (includeStrings) {
+    regex = /\d|one|two|three|four|five|six|seven|eight|nine/g
+  }
 
-  let matches = [];
-  let cursor = 0;
+  let matches = []
+  let cursor = 0
 
   // to deal with overlaps, we use a simple cursor
   while (cursor < line.length) {
-    let hits = line.substring(cursor).match(regex);
-    if (hits) matches.push(hits[0]);
-    cursor++;
+    const hits = line.substring(cursor).match(regex)
+    if (hits) matches.push(hits[0])
+    cursor++
   }
 
-  if (!matches) return;
+  if (!matches) return
 
   // convert any found words to single digits
-  matches = matches.map((match) => wordToDigit(match));
+  matches = matches.map(match => wordToDigit(match))
 
   // combine the digits to form a number
-  return Number(matches[0] + matches[matches.length - 1]);
+  return Number(matches[0] + matches[matches.length - 1])
 }
 
-function wordToDigit(word) {
+function wordToDigit (word) {
   const words = {
-    one: "1",
-    two: "2",
-    three: "3",
-    four: "4",
-    five: "5",
-    six: "6",
-    seven: "7",
-    eight: "8",
-    nine: "9",
-  };
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9'
+  }
 
-  return words.hasOwnProperty(word) ? words[word] : word;
+  return word in words ? words[word] : word
 }
--- 02/script.js
+++ 02/script.js
@@ -1,55 +1,53 @@
-const startTime = performance.now();
+const startTime = performance.now()
 
-const readline = require("readline");
-const fs = require("fs");
+const readline = require('readline')
+const fs = require('fs')
 
-const fileStream = fs.createReadStream("input");
+const fileStream = fs.createReadStream('input')
 
 const rl = readline.createInterface({
-  input: fileStream,
-});
+  input: fileStream
+})
 
-let [sum, sum2] = [0, 0];
-const totalDiceInBag = { red: 12, green: 13, blue: 14 };
+let [sum, sum2] = [0, 0]
+const totalDiceInBag = { red: 12, green: 13, blue: 14 }
 
-rl.on("line", (game) => {
-  let gameId = Number(game.substring(5).split(":")[0]);
-  let [possible, power] = isGamePossible(game);
-  if (possible) sum += gameId;
-  sum2 += power;
-});
+rl.on('line', game => {
+  const gameId = Number(game.substring(5).split(':')[0])
+  const [possible, power] = isGamePossible(game)
+  if (possible) sum += gameId
+  sum2 += power
+})
 
-function isGamePossible(game) {
-  const minimumDiceNeeded = [];
-  let possible = true;
-  let minNeeded = [];
-  sets = game.split(": ")[1].split("; ");
-  sets.forEach((set) => {
-    parts = set.split(", ");
-    parts.forEach((part) => {
-      let numberOfDice = Number(part.split(" ")[0]);
-      let diceColor = part.split(" ")[1];
-      if (numberOfDice > totalDiceInBag[diceColor]) possible = false;
+function isGamePossible (game) {
+  const minimumDiceNeeded = []
+  let possible = true
+  const sets = game.split(': ')[1].split('; ')
+  sets.forEach(set => {
+    const parts = set.split(', ')
+    parts.forEach(part => {
+      const numberOfDice = Number(part.split(' ')[0])
+      const diceColor = part.split(' ')[1]
+      if (numberOfDice > totalDiceInBag[diceColor]) possible = false
       if (
         !minimumDiceNeeded[diceColor] ||
         minimumDiceNeeded[diceColor] < numberOfDice
-      )
-        minimumDiceNeeded[diceColor] = numberOfDice;
-    });
-  });
+      ) {
+        minimumDiceNeeded[diceColor] = numberOfDice
+      }
+    })
+  })
 
   // we also need to return the power of the minimum dice needed
   return [
     possible,
-    minimumDiceNeeded["red"] *
-      minimumDiceNeeded["blue"] *
-      minimumDiceNeeded["green"],
-  ];
+    minimumDiceNeeded.red * minimumDiceNeeded.blue * minimumDiceNeeded.green
+  ]
 }
 
-rl.on("close", () => {
-  console.log("answer:", sum, sum2);
-  const endTime = performance.now();
-  const runtimeMs = endTime - startTime;
-  console.log(`runtime: ${runtimeMs} ms`);
-});
+rl.on('close', () => {
+  console.log('answer:', sum, sum2)
+  const endTime = performance.now()
+  const runtimeMs = endTime - startTime
+  console.log(`runtime: ${runtimeMs} ms`)
+})
